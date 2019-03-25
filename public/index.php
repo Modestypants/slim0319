db78b40eade35245707c3c5a393a3015dffd3a06
Modestypants <imane@imane.com>
2019-03-25 14:53:03 +0100

    php sniffer

--- public/index.php
+++ public/index.php
@@ -1,2 +1,21 @@
 <?php
-echo "Bonjour";
+
+//Autoloader
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// creation de l'application
+$app = new App();
+
+// Creation d'une route
+$app->get("/hello", function (ServerRequestInterface $request, ResponseInterface $response, ?array $arg) {
+
+    // on retourne une reponse
+    return $response->getBody()->write('<h1>Bonjour</h1>');
+});
+
+// création et renvoi de la reponse au navigateur
+$app->run();
